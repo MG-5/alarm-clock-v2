746b2ad97d5dff1bb52bc8bb5266ca0e900c409e
MG-5 <MG-5@users.noreply.github.com>
2017-10-30 20:45:07 +0100

    ignore compiler files

--- Wecker 2.0/Debug/RGB_vector.d
+++
@@ -1,9 +0,0 @@
-RGB_vector.d RGB_vector.o: .././RGB_vector.cpp .././RGB_vector.h \
- d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
-
-.././RGB_vector.h:
-
-d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
-
-d:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
